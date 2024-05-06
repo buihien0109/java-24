dd7ae77280ad1c8881b9eab5ad1ff7eb9db3d4c3
buihien0109 <buihien01091997@gmail.com>
2024-05-06 22:37:51 +0700

    update

--- day_09/course-management/src/main/java/com/example/demo/course/exception/ResouceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.course.exception;
-
-public class ResouceNotFoundException extends RuntimeException {
-    public ResouceNotFoundException(String message) {
-        super(message);
-    }
-}
--- day_09/course-management/src/main/java/com/example/demo/course/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.course.service;
-
-import com.example.demo.course.model.User;
-
-import java.util.List;
-
-public interface UserService {
-    List<User> getAllUser();
-}
