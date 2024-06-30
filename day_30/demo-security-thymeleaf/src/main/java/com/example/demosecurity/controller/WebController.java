4fe2874e9c8ff6493142d0c2f96d066ccf90665c
buihien0109 <buihien01091997@gmail.com>
2024-06-30 10:07:59 +0700

    update

---
+++ day_30/demo-security-thymeleaf/src/main/java/com/example/demosecurity/controller/WebController.java
@@ -0,0 +1,27 @@
+package com.example.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+    @GetMapping("/")
+    public String getHome() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String getUser() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String getAdmin() {
+        return "admin";
+    }
+
+    @GetMapping("/login")
+    public String getLogin() {
+        return "login";
+    }
+}
---
+++ day_30/demo-security-thymeleaf/src/main/java/com/example/demosecurity/security/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.demosecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // Cấu hình đường dẫn
+        String[] publicRoutes = new String[]{"/css/**", "/js/**", "/images/**"};
+        http.authorizeHttpRequests(auth -> {
+            auth.requestMatchers(publicRoutes).permitAll();
+            auth.requestMatchers("/").permitAll();
+            auth.requestMatchers("/user").hasRole("USER");
+            auth.requestMatchers("/admin").hasRole("ADMIN");
+            auth.requestMatchers("/aa/**", "/bb/**").hasAnyRole("USER", "ADMIN");
+            auth.requestMatchers(HttpMethod.POST, "/cc/**").hasRole("AUTHOR");
+            auth.requestMatchers(HttpMethod.GET, "/dd/**").hasAuthority("ROLE_USER");
+            auth.requestMatchers("/abc/**", "/xyz/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN");
+            auth.anyRequest().authenticated();
+        });
+
+        // Cấu hình form login
+        http.formLogin(form -> {
+            form.loginPage("/login");
+            form.loginProcessingUrl("/login-process");
+            form.usernameParameter("email");
+            form.passwordParameter("pass");
+            form.defaultSuccessUrl("/", true);
+            form.permitAll();
+        });
+
+        // Cấu hình logout
+        http.logout(logout -> {
+            logout.logoutSuccessUrl("/");
+            logout.invalidateHttpSession(true);
+            logout.deleteCookies("JSESSIONID");
+            logout.clearAuthentication(true);
+            logout.permitAll();
+        });
+
+        return http.build();
+    }
+}
