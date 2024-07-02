9ff9d03e52323b58fec87783aa4a0cd91e394ca9
buihien0109 <buihien01091997@gmail.com>
2024-07-02 20:54:26 +0700

    update

--- day_30/demo-security-thymeleaf/src/main/java/com/example/demosecurity/security/SecurityConfig.java
+++ day_30/demo-security-thymeleaf/src/main/java/com/example/demosecurity/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.demosecurity.security;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -41,7 +43,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
+        http.csrf(AbstractHttpConfigurer::disable);
 
         // Cấu hình đường dẫn
         http.authorizeHttpRequests(auth -> {
@@ -55,6 +57,17 @@ public class SecurityConfig {
             logout.deleteCookies("JSESSIONID");
             logout.clearAuthentication(true);
             logout.permitAll();
+        });
+
+        // Cấu hình xử lý lỗi
+        http.exceptionHandling(exceptionHandling -> {
+            exceptionHandling.authenticationEntryPoint((request, response, authException) -> {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+            });
+
+            exceptionHandling.accessDeniedHandler((request, response, accessDeniedException) -> {
+                response.sendError(HttpServletResponse.SC_FORBIDDEN, "Forbidden");
+            });
         });
 
         // Cấu hình xác thục
